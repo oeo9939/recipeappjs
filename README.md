fb39468656431acbe6227756a4b83f1923434db3
Teo <mgttmich@gmail.com>
2022-11-16 22:57:55 +0200

    Set fetch for randomMeal, MealById, MealBySearch

--- README.md
+++ README.md
@@ -3,3 +3,17 @@
 https://uidesigndaily.com/posts/sketch-recipe-app-food-mobile-day-615
 
 https://www.themealdb.com/api.php
+
+fetch:
+
+getRandomMeal() {
+    www.themealdb.com/api/json/v1/1/random.php
+}
+
+getMealById(id) {
+    www.themealdb.com/api/json/v1/1/lookup.php?i=52772
+}
+
+getMealsBySearch(term) {
+    www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata
+}
---
+++ main.js
@@ -0,0 +1,15 @@
+getRandomMeal();
+
+async function getRandomMeal() {
+    const resp = await fetch("https://www.themealdb.com/api/json/v1/1/random.php");
+    const randomMeal = await resp.json();
+    console.log(randomMeal);
+}
+
+async function getMealById(id) {
+    const meal = await fetch('https://www.themealdb.com/api/json/v1/1/lookup.php?i='+id);
+}
+
+async function getMealsBySearch(term) {
+    const meals = await fetch('https://www.themealdb.com/api/json/v1/1/search.php?s='+term);
+}
